c5dbb59d0f51953543d579ef8405976c170d3675
yang <everymoment.studio@gmail.com>
2017-07-20 14:03:59 +0800

    custom theme vtheme

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'wptest');
 define('DB_PASSWORD', '07010020');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '139.199.109.247');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -77,7 +77,7 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* That's all, stop editing! Happy blogging. */
 
